6ea212e7bdb862f35b8bd6e8919bbc323251411b
you-1257 <you-1257@qq.com>
2026-07-22 23:30:13 +0800

    chore: 更新启动类配置

--- backend/src/main/java/com/lanyuan/starter/StarterApplication.java
+++ backend/src/main/java/com/lanyuan/starter/StarterApplication.java
@@ -1,13 +1,23 @@
 package com.lanyuan.starter;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
 @SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
 @OpenAPIDefinition(info = @Info(title = "榄园知行 Starter API", version = "0.1.0", description = "团队练习项目公共基线"))
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
 public class StarterApplication {
     public static void main(String[] args) { SpringApplication.run(StarterApplication.class, args); }
 }
