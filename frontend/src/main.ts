783bbbc87e2fe37399fd71ba614405215f466547
anlin <397620663@qq.com>
2026-07-23 11:17:41 +0800

    完成登录页面

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,7 +3,7 @@ import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import 'element-plus/dist/index.css'
-import './styles-fixed.css'
+import './styles.css'
 import App from './App.vue'
 import router from './router'
 
